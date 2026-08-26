ca580e30dd096cf7148f99167aa3cf3136ec11f9
Antonio-Reis <antoniofont94@gmail.com>
2026-08-26 11:02:16 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,39 @@
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+import pygame
+from logger import log_state
+from player import Player
+
+def main():
+    print(f"Starting Asteroids with pygame version {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}") 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    #creating groups
+    updatable = pygame.sprite.Group()
+    drawable =  pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player =Player(x,y)
+
+    # GAME LOOP
+    while True:
+        log_state()
+        for event in pygame.event.get():
+	        if event.type == pygame.QUIT:
+                    return
+        updatable.update(dt)
+        #start drawing
+        screen.fill('black')
+        for draw in drawable:
+            draw.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
+       
+
+
+
+if __name__ == "__main__":
+    main()
